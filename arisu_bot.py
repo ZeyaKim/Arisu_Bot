d1e4419d0465712cfbce3bf31bb059902df89614
ZeyaKim <chlwltjrdev@gmail.com>
2023-05-25 16:17:50 +0900

    feat 끄앙

--- arisu_bot.py
+++ arisu_bot.py
@@ -11,5 +11,11 @@ bot = commands.Bot(command_prefix='`', intents=intents)  # Intents 추가
 
 imagePath = './images'
 
+@bot.command()
+async def 끄앙(ctx):
+    with open(os.path.join(imagePath, 'cry.jpg'), 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+        await ctx.send('끄앙!')
 
 bot.run(TOKEN)
