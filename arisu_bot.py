f6e9c8a34320418bbb1558395fcb5d51dc1b0db9
ZeyaKim <chlwltjrdev@gmail.com>
2023-05-25 12:57:00 +0900

    add debug message

--- arisu_bot.py
+++ arisu_bot.py
@@ -3,12 +3,14 @@ from discord.ext import commands
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+print(f'token = {TOKEN}')
 
 intents = discord.Intents.default()  # 기본 Intents 모두 활성화
 bot = commands.Bot(command_prefix='`', intents=intents)  # Intents 추가
 
 @bot.command()
 async def hello(ctx):
+    print('hello?')
     await ctx.send('Hello!')
 
 bot.run(TOKEN)
