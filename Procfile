b8df9979fd5ee6c96fcbd2cee8d53c20d3c17590
ZeyaKim <chlwltjrdev@gmail.com>
2023-05-25 12:40:01 +0900

    delete Procfile

--- Procfile
+++
@@ -1,1 +0,0 @@
-worker: python arisu_bot.py
--- arisu_bot.py
+++ arisu_bot.py
@@ -2,11 +2,10 @@ import discord
 from discord.ext import commands
 import os
 
-
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix= '`')
-
+intents = discord.Intents.default()  # 기본 Intents 모두 활성화
+bot = commands.Bot(command_prefix='`', intents=intents)  # Intents 추가
 
 @bot.command()
 async def hello(ctx):
