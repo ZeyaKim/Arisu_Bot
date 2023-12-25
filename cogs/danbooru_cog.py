b39599a2af9711e503df63e6e28a70e55144d717
ZeyaKim <chlwltjrdev@gmail.com>
2023-12-25 18:19:29 +0900

    feat: 짤검색

--- cogs/danbooru_cog.py
+++ cogs/danbooru_cog.py
@@ -1,9 +1,8 @@
-import requests
-import os
 import aiohttp
 import asyncio
 from discord.ext import commands
 import discord
+import os
 
 class DanbooruCog(commands.Cog):
     def __init__(self, bot):
@@ -13,6 +12,10 @@ class DanbooruCog(commands.Cog):
 
     async def download_file(self, session, url, file_name):
         async with session.get(url) as response:
+            if response.status != 200:
+                # 에러 처리
+                print(f"Error downloading {url}: Status code {response.status}")
+                return
             content = await response.read()
             self.write_file(file_name, content)
 
@@ -29,67 +32,73 @@ class DanbooruCog(commands.Cog):
 
     @commands.command()
     async def 짤(self, ctx):
-        if self.is_while_seraching:
+        if self.is_while_searching:
             await ctx.send("다른 짤을 먼저 찾는 중입니다..")
             return
-        self.is_while_seraching = True
+        self.is_while_searching = True
         try:
             params = self.make_params("", 200)
-            response = requests.get(self.url, params=params)
-            if response.status_code != 200:
-                await ctx.send("아리스가 짤을 찾지 못했습니다...")
-                return
-            posts = response.json()
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.url, params=params) as response:
+                    if response.status != 200:
+                        await ctx.send("아리스가 짤을 찾지 못했습니다...")
+                        return
+                    posts = await response.json()
 
-            sorted_posts = sorted(posts, key=lambda post: post['score'], reverse=True)[:10]
-            url_list = [post['file_url'] for post in sorted_posts]
-            ext = [url.split('.')[-1] for url in url_list]
-            file_name_list = [f"SPOILER_image{idx}.{ext}" for idx, ext in enumerate(ext)]
-            
+                    posts = [post for post in posts if 'file_url' in post]
+                    sorted_posts = sorted(posts, key=lambda post: post['score'], reverse=True)[:10]
+                    url_list = [post['file_url'] for post in sorted_posts]
+                    ext = [url.split('.')[-1] for url in url_list]
+                    file_name_list = [f"SPOILER_image{idx}.{ext}" for idx, ext in enumerate(ext)]
 
-            files = [discord.File(file_name, filename=file_name) for file_name in file_name_list]
-            await ctx.send("아리스가 짤을 모아왔습니다!", files=files)
+                    download_tasks = [self.download_file(session, url, file_name) 
+                                      for url, file_name in zip(url_list, file_name_list)]
+                    await asyncio.gather(*download_tasks)
 
-            for file_name in file_name_list:
-                os.remove(file_name)
-                
+                    files = [discord.File(file_name, filename=file_name) for file_name in file_name_list]
+                    await ctx.send("아리스가 짤을 모아왔습니다!", files=files)
+
+                    for file_name in file_name_list:
+                        os.remove(file_name)
         finally:
-            self.is_while_seraching = False
+            self.is_while_searching = False
 
     @commands.command()
     async def 짤검색(self, ctx, *args):
-        if self.is_while_seraching:
+        if len(args) >= 3:
+            await ctx.send("3개 이상의 태그는 검색할 수 없습니다!")
+            return
+
+        if self.is_while_searching:
             await ctx.send("다른 짤을 먼저 찾는 중입니다..")
             return
         
-        self.is_while_seraching = True
+        self.is_while_searching = True
         try:    
             tags = ' '.join(args)
             params = self.make_params(tags, 200)
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.url, params=params) as response:
+                    if response.status != 200:
+                        await ctx.send("아리스가 짤을 찾지 못했습니다...")
+                        return
+                    posts = await response.json()
 
-            response = requests.get(self.url, params=params)
-            if response.status_code != 200:
-                await ctx.send("아리스가 짤을 찾지 못했습니다...")
-                return
+                    posts = [post for post in posts if 'file_url' in post]
+                    sorted_posts = sorted(posts, key=lambda post: post['score'], reverse=True)[:10]
+                    url_list = [post['file_url'] for post in sorted_posts]
+                    ext = [url.split('.')[-1] for url in url_list]
+                    file_name_list = [f"SPOILER_image{idx}.{ext}" for idx, ext in enumerate(ext)]
 
-            posts = response.json()
-            sorted_posts = sorted(posts, key=lambda post: post['score'], reverse=True)[:10]
-            url_list = [post['file_url'] for post in sorted_posts]
-            extension = [url.split('.')[-1] for url in url_list]
-            file_name_list = [f"SPOILER_image{idx}.{ext}" for idx, ext in enumerate(extension)]
+                    download_tasks = [self.download_file(session, url, file_name) 
+                                      for url, file_name in zip(url_list, file_name_list)]
+                    await asyncio.gather(*download_tasks)
 
-            download_tasks = [self.download_file(url, file_name)
-                                for url, file_name in zip(url_list, file_name_list)]
-            await asyncio.gather(*download_tasks)
+                    files = [discord.File(file_name, filename=file_name) for file_name in file_name_list]
+                    await ctx.send("아리스가 짤을 모아왔습니다!", files=files)
 
-            files = [discord.File(file_name, filename=file_name) for file_name in file_name_list]
-            await ctx.send("아리스가 짤을 모아왔습니다!", files=files)
-
-            for file_name in file_name_list:
-                os.remove(file_name)
-
-        finally:
-            self.is_while_seraching = False
-
-async def setup(bot):
-    await bot.add_cog(DanbooruCog(bot))
+                    for file_name in file_name_list:
+                        os.remove(file_name)
+        except Exception as e:
+            print(e)
+            await ctx
